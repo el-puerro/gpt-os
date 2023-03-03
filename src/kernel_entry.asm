ea48630262643bd31d9d24b9d5cdd54ba14d6d98
Lucy Mielke <mielkesteven@icloud.com>
2023-03-03 13:48:08 +0100

    Make kernel bootable by GRUB
    This patch makes the kernel Bootable. there were a number of errors,
    that prevent it from booting, but it's more or less working now.
    vga_printf is still broken tho.
    
    Didn't think Chat-GPT3 could get this far tbh
    
    Signed-off-by: Lucy Mielke <mielkesteven@icloud.com>

--- src/kernel_entry.asm
+++ src/kernel_entry.asm
@@ -1,38 +1,40 @@
 ; kernel_entry.asm - entry point and bootloader information
-; created 2023-03-02 @ el-perro
+; created 2023-03-02 @ el-puerro
 ;
 ; updates:
-; 
+;	2023-03-03 @ el-puerro: make bootable by grub
 
+[bits 32]
+
+; define multiboot header
+align 4
+multiboot_header:
+	dd 0x1BADB002		; magic number
+	dd 0			; flags
+	dd - (0x1BADB002 + 0)	; checksum
+
+; kernel entry point 
 global kernel_entry
 extern kernel_main
 
 section .text
-KERNEL_STACK_SIZE	equ 0x4000
-MULTIBOOT_MAGIC		equ 0x1BADB002
-MULTIBOOT_HEADER	equ kernel_entry - 0x1000
 
 kernel_entry:
-	; set up the stack pointer 
+	; set up stack pointer
 	mov esp, stack_space + KERNEL_STACK_SIZE
 
-	; load multiboot magic number into eax
-	mov eax, MULTIBOOT_MAGIC
-
-	; Load multiboot header into ebx
-	mov ebx, MULTIBOOT_HEADER
-
-	; main kernel entry point from kernel_main.c
+	; call kernel_main
 	call kernel_main
 
-	; if the kernel unexpectedly returns, halt the CPU
+	; if the kernel unexpectedly returns, halt the cpu
 	cli
+	hlt
 
-halt_loop:
-	halt
-	jmp halt_loop
-
-; Setup the stack
+; set up the stack
 section .bss
 align 4
-stack_space resb KERNEL_STACK_SIZE
+stack_space: 
+	resb KERNEL_STACK_SIZE
+
+KERNEL_STACK_SIZE equ 0x4000
+
