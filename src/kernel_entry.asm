209b81adf11967e63092bdbf1199d69b1747d228
Lucy Mielke <mielkesteven@icloud.com>
2023-03-05 15:11:59 +0100

    Initialise GDT before initialising the kernel
    Signed-off-by: Lucy Mielke <mielkesteven@icloud.com>

--- src/kernel_entry.asm
+++ src/kernel_entry.asm
@@ -3,6 +3,7 @@
 ;
 ; updates:
 ;	2023-03-03 @ el-puerro: make bootable by grub
+; 	2023-03-05 @ chatGPT: GDT initialization
 
 [bits 32]
 
@@ -13,15 +14,43 @@ multiboot_header:
 	dd 0			; flags
 	dd - (0x1BADB002 + 0)	; checksum
 
-; kernel entry point 
+; kernel entry point
 global kernel_entry
 extern kernel_main
+
+; GDT initialization
+gdt_start:
+	dd 0x00000000		; null descriptor
+	dd 0x00000000
+gdt_code:
+	dw 0xFFFF		; limit low
+	dw 0x0000		; base low
+	db 0x00			; base middle
+	db 0x9A			; access
+	db 0xCF			; granularity
+	db 0x00			; base high
+gdt_data:
+	dw 0xFFFF		; limit low
+	dw 0x0000		; base low
+	db 0x00			; base middle
+	db 0x92			; access
+	db 0xCF			; granularity
+	db 0x00			; base high
+gdt_end:
+
+gdt_descriptor:
+	dw gdt_end - gdt_start - 1	; GDT limit
+	dd gdt_start			; GDT base
 
 section .text
 
 kernel_entry:
+
 	; set up stack pointer
 	mov esp, stack_space + KERNEL_STACK_SIZE
+
+	; initialize GDT
+	lgdt [gdt_descriptor]
 
 	; call kernel_main
 	call kernel_main
@@ -33,8 +62,7 @@ kernel_entry:
 ; set up the stack
 section .bss
 align 4
-stack_space: 
+stack_space:
 	resb KERNEL_STACK_SIZE
 
 KERNEL_STACK_SIZE equ 0x4000
-
