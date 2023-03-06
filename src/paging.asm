77b238ee424643bb9499c28134d233b397e3bcac
Lucy Mielke <mielkesteven@icloud.com>
2023-03-06 14:48:25 +0100

    Staging still doesn't wanna work

--- src/paging.asm
+++ src/paging.asm
@@ -1,42 +1,52 @@
-; src/paging.asm
-; created 2023-03-06 @ ChatGPT
-
 global paging_init
+
+section .bss
+align 4096
+page_directory:
+    resb 4096
+page_table:
+    resb 4096
 
 section .text
 
 paging_init:
-	; set up page directory
-	mov eax, 0x1000     ; allocate a page for the page directory
-	mov cr3, eax        ; load the page directory address into CR3
-	mov eax, 0x101000   ; set eax to point to the beginning of the page directory
-	mov ebx, 0x103     ; set ebx to point to the beginning of the page table
-	mov ecx, 1024      ; loop through all entries in the page directory
-.loop_pd:
-	mov edx, ebx        ; load the value at ebx into edx
-	or edx, 3          ; set the present bit and read/write bit
-	mov [eax], edx      ; store the value back into memory
-	add eax, 4         ; advance to next entry in page directory
-	add ebx, 4096      ; advance to next page table
-	dec ecx            ; decrement loop counter
-	jnz .loop_pd       ; loop if not done
+    ; map first 1MB with identity mapping
+    mov eax, 0
+    mov ebx, 0
+.loop_identity_map:
+    mov dword [page_table + ebx], eax
+    or dword [page_table + ebx], 3   ; set present bit and read/write bit
+    add eax, 4096
+    add ebx, 4
+    cmp ebx, 4096
+    jl .loop_identity_map
 
-	; set up page table
-	mov eax, 0x103000   ; set eax to point to the beginning of the page table
-	mov ebx, 0          ; clear ebx
-	mov ecx, 1024       ; loop through all entries in the page table
-.loop_pt:
-	mov edx, ebx        ; load the value at ebx into edx
-	or edx, 3          ; set the present bit and read/write bit
-	mov [eax], edx      ; store the value back into memory
-	add eax, 4          ; advance to next entry in page table
-	add ebx, 4096       ; advance to next physical page
-	dec ecx             ; decrement loop counter
-	jnz .loop_pt        ; loop if not done
+    ; map higher half kernel
+    mov eax, 0xC0100000   ; map physical address 0x100000 to virtual address 0xC0100000
+    mov ebx, 0x100000
+.loop_higher_half:
+    mov dword [page_table + ebx], eax
+    or dword [page_table + ebx], 3   ; set present bit and read/write bit
+    add eax, 4096
+    add ebx, 4
+    cmp ebx, 0x200000
+    jl .loop_higher_half
 
-	; enable paging
-	mov eax, cr0
-	or eax, 0x80000000  ; set the paging bit in CR0
-	mov cr0, eax
+    ; create page directory entry for page table
+    mov eax, page_table
+    mov ebx, 0xC0000000 | 3   ; set present bit, read/write bit, and user/supervisor bit
+    mov dword [page_directory + 768*4], ebx
+    or dword [page_directory + 768*4], eax
 
-	ret
+    ; set up page directory
+    mov ebx, page_directory
+    mov eax, ebx
+    or eax, 3   ; set present bit and read/write bit
+    mov cr3, eax
+
+    ; enable paging
+    mov eax, cr0
+    or eax, 0x80000000   ; set the paging bit in CR0
+    mov cr0, eax
+
+    ret
